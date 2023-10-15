429c1e04fdd326dc043e2118b7de3c0123df17a7
geedy <dante.gamecreator@gmail.com>
2023-10-16 02:58:59 +0300

    6.6.1

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NewsList
+from .views import *
 
 
 urlpatterns = [
    path('', NewsList.as_view()),
+   path('news/<int:pk>/', NewDetail.as_view(), name='new_detail'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+from django.core.cache import cache
 
 
 class NewsList(ListView):
@@ -8,3 +9,16 @@ class NewsList(ListView):
     categoryType = 'NS'
     template_name = 'news.html'
     context_object_name = 'news'
+
+class NewDetail(DetailView):
+    model = Post
+    template_name = 'Full_new.html'
+    context_object_name = 'new'
+
+    def get_object(self, *args, **kwargs):
+        obj = cache.get(f'post-{self.kwargs["pk"]}', None)
+        if not obj:
+            obj = super().get_object(queryset=self.queryset)
+            cache.set(f'post-{self.kwargs["pk"]}', obj)
+        return obj
+
